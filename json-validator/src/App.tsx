f8899989a0a40ec6b7de7faafac1a1e9b5dce030
Eliza Sohn <eliza.sohn@gmail.com>
2024-09-19 12:38:57 -0700

    scaffolding

---
+++ json-validator/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          </header>
+          <div>
+              <iframe src="https://lottie.host/embed/e3f96da8-7914-4678-8c0c-6e0bbfc04336/X9qeP7lNfG.json" width={500} height={500}></iframe>
+          </div>
+    </div>
+  );
+}
+
+export default App;
